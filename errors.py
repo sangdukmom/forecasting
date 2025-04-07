4de1c2e37a123e2b0bd07acc852174e18f778370
sangdukmom <116548265+sangdukmom@users.noreply.github.com>
2025-04-07 09:02:31 +0900

    Update errors.py

--- errors.py
+++ errors.py
@@ -1,30 +1,30 @@
 ---------------------------------------------------------------------------
-KeyError                                  Traceback (most recent call last)
-File c:\Users\SKSiltron\Desktop\project\signpost_project\signpost_project_env\Lib\site-packages\pandas\core\indexes\base.py:3805, in Index.get_loc(self, key)
-   3804 try:
--> 3805     return self._engine.get_loc(casted_key)
-   3806 except KeyError as err:
+ValueError                                Traceback (most recent call last)
+File c:\Users\SKSiltron\Desktop\project\signpost_project\signpost_project_env\Lib\site-packages\pandas\core\indexes\base.py:565, in Index.__new__(cls, data, dtype, copy, name, tupleize_cols)
+    564 try:
+--> 565     arr = sanitize_array(data, None, dtype=dtype, copy=copy)
+    566 except ValueError as err:
 
-File index.pyx:167, in pandas._libs.index.IndexEngine.get_loc()
+File c:\Users\SKSiltron\Desktop\project\signpost_project\signpost_project_env\Lib\site-packages\pandas\core\construction.py:659, in sanitize_array(data, index, dtype, copy, allow_2d)
+    657             subarr = maybe_infer_to_datetimelike(subarr)
+--> 659 subarr = _sanitize_ndim(subarr, data, dtype, index, allow_2d=allow_2d)
+    661 if isinstance(subarr, np.ndarray):
+    662     # at this point we should have dtype be None or subarr.dtype == dtype
 
-File index.pyx:196, in pandas._libs.index.IndexEngine.get_loc()
+File c:\Users\SKSiltron\Desktop\project\signpost_project\signpost_project_env\Lib\site-packages\pandas\core\construction.py:718, in _sanitize_ndim(result, data, dtype, index, allow_2d)
+    717         return result
+--> 718     raise ValueError(
+    719         f"Data must be 1-dimensional, got ndarray of shape {data.shape} instead"
+    720     )
+    721 if is_object_dtype(dtype) and isinstance(dtype, ExtensionDtype):
+    722     # i.e. NumpyEADtype("O")
 
-File pandas\\_libs\\hashtable_class_helper.pxi:7081, in pandas._libs.hashtable.PyObjectHashTable.get_item()
-
-File pandas\\_libs\\hashtable_class_helper.pxi:7089, in pandas._libs.hashtable.PyObjectHashTable.get_item()
-
-KeyError: 'date'
+ValueError: Data must be 1-dimensional, got ndarray of shape (420, 4) instead
 
 The above exception was the direct cause of the following exception:
-
-KeyError                                  Traceback (most recent call last)
-Cell In[7], line 2
-      1 # 실행
-----> 2 process_and_forecast(INPUT_FILES['seasonal'], 'seasonal')
-      3 process_and_forecast(INPUT_FILES['trend'], 'trend')
 ...
-   3815     #  InvalidIndexError. Otherwise we fall through and re-raise
-   3816     #  the TypeError.
-   3817     self._check_indexing_error(key)
+--> 570         raise ValueError("Index data must be 1-dimensional") from err
+    571     raise
+    572 arr = ensure_wrapped_if_datetimelike(arr)
 
-KeyError: 'date'
+ValueError: Index data must be 1-dimensional
